bd935ac488e991fd74ce18472e284c42f9030850
bouquet1 <bukuleta@gmail.com>
2023-08-23 20:34:38 -0700

    table created

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,26 @@
 from config import CONN, CURSOR
+import sqlite3
 
+CONN = sqlite3.connect('music.db')
+CURSOR = CONN.cursor()
+
+#to "map" our class to a database table, create a table with the same name as our class and give that table column names that match the instance attributes of the class.
 class Song:
-    pass
+    
+    def __init__ (self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+
