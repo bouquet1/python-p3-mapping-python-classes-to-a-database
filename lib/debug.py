8aed7c3f8f2d7efe4f067c76d668299f72778495
bouquet1 <bukuleta@gmail.com>
2023-08-23 21:13:37 -0700

    creating table rows

--- lib/debug.py
+++ lib/debug.py
@@ -6,3 +6,6 @@ from song import Song
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+    #In this file, we're importing in the sqlite3.Connection and sqlite3.Cursor objects that we instantiated in lib/config.py. We're also importing the Song class so that we can use its methods during our pdb session.
+
+    
--- lib/song.py
+++ lib/song.py
@@ -4,14 +4,16 @@ import sqlite3
 CONN = sqlite3.connect('music.db')
 CURSOR = CONN.cursor()
 
-#to "map" our class to a database table, create a table with the same name as our class and give that table column names that match the instance attributes of the class.
+#to "map" our class to a database table, create a table with the same name as our class and give that table column names that match the instance attributes (name, album) of the class.
 class Song:
-    
+
     def __init__ (self, name, album):
-        self.id = None
+        self.id = None #to let SQL assign unique id
         self.name = name
         self.album = album
 
+
+#crafts a SQL statement to create a songs table and give that table column names that match the attributes of an individual instance of Song.
     @classmethod
     def create_table(cls):
         sql = """
@@ -23,4 +25,16 @@ class Song:
         """
 
         CURSOR.execute(sql)
+    
+#INSERT INTO songs (name, album) VALUES ("Hello", "25"); 
+#When save a record, we are repeating the same exact steps and using the same code. The only things that are different are the values that we are inserting into our songs table. Let's abstract this functionality into an instance method, save(),that saves a given instance of our Song class into the songs table of our database..
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
 
+        CURSOR.execute(sql, (self.name, self.album))
+
+    # Commit the changes to the database
+    # db_connection.commit()
